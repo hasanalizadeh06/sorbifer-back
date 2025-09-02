f0430e5190fe8abc9b59a53c03b86c7951420316
HasanAlizadeh <texnovideo181@gmail.com>
2025-09-02 16:39:24 +0400

    deployment fix cros

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,9 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3001', // veya frontend adresiniz
-    credentials: true,
+    origin: ['https://sobifer.vercel.app'],
+    credentials: true, // Eğer cookie kullanıyorsanız
   });
-
   const config = new DocumentBuilder()
     .setTitle('Your API')
     .setDescription('API description')
